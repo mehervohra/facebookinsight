2d2ae700053e4b12c9e94d827fcd5f1ef5c290d4
mmjenjen <mjennin3@wellesley.edu>
2016-02-20 14:33:08 -0500

    Update mj_test.js

--- mj_test.js
+++ mj_test.js
@@ -1,11 +1,16 @@
 /* testing indico functions */
 
-var data = document.getElementById("in").val();
+
 
 // single example
-$.post(
+
+$('button').click(display);
+  
+function display(){
+  var data = $("in").val();
+  $.post(
   'https://apiv2.indico.io/sentiment?key=680e518e7ad121cdecba4a3a56d1bfe7',
   JSON.stringify(data)
 ).then(function(res) { console.log(res) });
-
-document.getElementById("result").innerHTML(res);
+  $('result').html(res);
+}
