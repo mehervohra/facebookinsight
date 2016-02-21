5111abc088eaafbc52bf78347ed3d8c9918a4dce
jackihom <jhom@wellesley.edu>
2016-02-21 01:13:56 -0500

    cleaned up data for sentiment and personality analysis

--- facebookinsight.js
+++ facebookinsight.js
@@ -7,7 +7,6 @@ if (Meteor.isClient) {
     if (response.status === 'connected') {
       fbAccessToken = response.authResponse.accessToken;
       testAPI();
-        getPosts(); // testing
     } else if (response.status === 'not_authorized') {
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
@@ -25,7 +24,7 @@ if (Meteor.isClient) {
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : '1957057311186486',
+    appId      : '1957057311186486', // unique key for each app
     cookie     : true,  // enable cookies to allow the server to access
                         // the session
     xfbml      : true,  // parse social plugins on this page
@@ -47,14 +46,13 @@ if (Meteor.isClient) {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
+
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-/*      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!'; */
+      getPosts();
+//      console.log('Thanks for logging in, ' + response.name + '!'); 
     });
   }
 
@@ -76,56 +74,58 @@ if (Meteor.isClient) {
           post = response.data[postIdx];
           if (post.message) {
             content += ' ' + post.message.replace(/(\r\n|\n|\r)/gm, ' ');
-            count++;
+//            count++;
           }
         }
-        console.log(response.paging.next);
+//        console.log(response.paging.next);
 //    }
       
     return content;
   }
 
+    // returns data on sentiment and personality
   function callIndico(content) {
     $.post(
-      'https://apiv2.indico.io/sentimenthq/batch?key=680e518e7ad121cdecba4a3a56d1bfe7',
+        'https://apiv2.indico.io/apis/multiapi/batch?key=680e518e7ad121cdecba4a3a56d1bfe7&apis=sentimenthq,personality',
       JSON.stringify({
         'data': [
           JSON.stringify(content)
         ]
       })
     ).then(function(res) {
+        console.log(res);
       var sortedResults = sortResults(JSON.parse(res));
       console.log('Sorted Results: ' + JSON.stringify(sortedResults));
       // TODO display the sorted results to the webpage!
     });
   }
 
-  function sortResults(results) {
-    var actualResults = results.results[0];
-    var sortable = [];
+    function sortResults(results) {
+        console.log(results);
+        sentiment = results.results.sentimenthq.results[0]; // type number
+        personality = results.results.personality.results[0]; // type Object
 
-    for (var keyword in actualResults) {
-      sortable.push([keyword, actualResults[keyword]]);
+        var sortable = [];
+
+        for (var keyword in results.results) {
+            sortable.push([keyword, results.results[keyword].results[0]]);
+        }
+        return sortable;
     }
+    
+    /*// counter starts at 0
+  Session.setDefault('counter', 0);*/
 
-    return sortable.sort(function(a, b) {
-      return b[1] - a[1]
-    });
-  }
-
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
+  /*Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
-  });
+  });*/
 
   Template.body.events({
     'submit .new-task': function (event) {
       event.preventDefault();
-      getPosts();
+      getPosts(); // TODO automate this!!
     }
   });
 }
